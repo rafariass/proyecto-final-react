bec99feb62e232bec2e7aa62c5ede54331188fe3
Raul Farias S <rfarias@chattigo.com>
2022-11-19 19:22:42 -0300

    view sumary product

--- src/layouts/ProductSummary.jsx
+++ src/layouts/ProductSummary.jsx
@@ -1,5 +1,29 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Context from '../services/Market.context';
+
 export default function ProductSummary() {
+  const { id } = useParams();
+  const { products } = useContext(Context);
+  const [productSelected, setProductSelected] = useState({});
+
+  useEffect(() => {
+    const data = products?.find((p) => p?.id === id);
+    setProductSelected(data);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id]);
+
+  const capitalize = ([initial, ...rest]) => initial.toUpperCase() + rest.join('');
+  const formatter = new Intl.NumberFormat('es-CL', { minimumFractionDigits: 0 });
+
   return (
-    <div>ProductSummary</div>
-  )
+    <>
+      <div className='container dashboard p-3'>
+        <h1 className='display-3'>{productSelected?.name && capitalize(productSelected?.name)}</h1>
+        <p className='lead text-center'>{productSelected?.desc}</p>
+        <img src={productSelected?.img} alt={productSelected?.name} className='img-sumary' />
+        <h2 className='m-2'>Precio: $ {formatter.format(productSelected?.price)}</h2>
+      </div>
+    </>
+  );
 }
